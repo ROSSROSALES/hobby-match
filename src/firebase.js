681d16e60e40180083207321e4fbe2076696959a
Ross Rosales <ross.erosales@gmail.com>
2024-09-25 21:30:17 -0400

    add firebaseConfig and dependencies

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,23 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import { collection, getFirestore } from 'firebase/firestore';
 
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID,
+    measurementId: process.env.REACT_APP_MEASUREMENTID
+};
+
 const app = initializeApp(firebaseConfig); // initializes connection to firebase
 const auth = getAuth(app); //get which user is authenticated
 const firestore = getFirestore(app); // returns firestore instance, Database Instance
 const querySnapshot = collection(firestore, "user") // grabs snapshot of collection, after updating from 'firebase/firestore/lite' to 'firebase/firestore'
 
 
-
+console.log(process.env.REACT_APP_APIKEY);
 const provider = new GoogleAuthProvider();
 
 function signInWithGoogle() {
@@ -27,6 +37,7 @@ function signInWithGoogle() {
         }))
         .catch((error) => {
             console.log(error)
+            console.log("this is sa test")
         });
 }
 
