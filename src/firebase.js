e632042425e3db58f53b77bcababd0a0709cafec
Ross Rosales <ross.erosales@gmail.com>
2024-10-04 16:16:24 -0400

    readd firebaseConfig

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,13 @@ import 'firebase/compat/firestore';
 import { collection, getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_APIKEY,
-    authDomain: process.env.REACT_APP_AUTHDOMAIN,
-    projectId: process.env.REACT_APP_PROJECTID,
-    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-    appId: process.env.REACT_APP_APPID,
-    measurementId: process.env.REACT_APP_MEASUREMENTID
+    apiKey: "AIzaSyAOnYfYcGgrz5CBwlSj3NTW-Rzo6hQ85A8",
+    authDomain: "anime-match-a5f94.firebaseapp.com",
+    projectId: "anime-match-a5f94",
+    storageBucket: "anime-match-a5f94.appspot.com",
+    messagingSenderId: "561233950151",
+    appId: "1:561233950151:web:84b1c7e0e5684b410a95e2",
+    measurementId: "G-WMRL0G5JYF"
 };
 
 const app = initializeApp(firebaseConfig); // initializes connection to firebase
